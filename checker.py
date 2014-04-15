ea4a52f2859b14781867eb976aefe2ad7a6f4aee
Guillaume Pouillet <nekresh@gmail.com>
2014-04-15 22:37:44 +0200

    Add fallback when shutil.which isn't available

--- checker.py
+++ checker.py
@@ -2,16 +2,16 @@
 
 class logger:
     def __init__(self):
-        self.success = 0
-        self.total = 0
+        self._success = 0
+        self._total = 0
 
     def success(self, job):
-        self.success += 1
-        self.total += 1
+        self._success += 1
+        self._total += 1
         print(job, "[OK]")
 
     def fail(self, job):
-        self.total += 1
+        self._total += 1
         print(job, "[FAIL]")
 
     def check(self, test, job):
@@ -21,7 +21,7 @@ class logger:
             self.fail(job)
 
     def __str__(self):
-        return("%s success on %s tests (%s%%)" % (self.success, self.total, self.success / self.total * 100.0))
+        return("%s success on %s tests (%s%%)" % (self._success, self._total, self._success / self._total * 100.0))
 
 class python_checker:
     def check(self, logger):
@@ -31,8 +31,23 @@ class python_checker:
 class program_checker:
     def check(self, logger):
         import shutil
-        for p in ["bash", "tcsh", "zsh", "firefox", "chromium", "google-chrome"]:
-            logger.check(shutil.which(p) is not None, p)
+        
+        # use shutil.which when present
+        if "which" in dir(shutil):
+            w = shutil.which
+        else: # fallback to custom made version if it doesn't
+            w = self.which
+
+        for p in ["bash", "tcsh", "zsh", "firefox", "chromium", "google-chrome", "gcc", "g++", "clang", "clang++"]:
+            logger.check(w(p) is not None, p)
+
+    def which(self, program):
+        import os
+
+        for path in [os.path.join(p, program) for p in os.environ["PATH"].split(":")]:
+            if os.access(path, os.X_OK):
+                return path
+        return None
 
 l = logger()
 c = python_checker()
