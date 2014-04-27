f9d7c33282b3678c69be620bfed28b141fcda1bc
Guillaume Pouillet <nekresh@gmail.com>
2014-04-27 12:13:42 +0200

    Add setuptools support for deployment

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='machine-checker',
+    version='0.1',
+    url='http://github.com/nekresh/machine-checker',
+    author_email='nekresh@gmail.com',
+    scripts=['checker.py'],
+)
