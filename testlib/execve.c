708ef280b74d454673b68246de6063e889fb8e7c
Guillaume Pouillet <nekresh@gmail.com>
2014-04-27 21:19:15 +0200

    Add few more tests for libC

---
+++ testlib/execve.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+int main(int ac, char** av)
+{
+    execve(NULL, NULL, NULL);
+    return (0);
+}
---
+++ testlib/printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int ac, char** av)
+{
+    printf("");
+    printf("%d", 10);
+    return (0);
+}
