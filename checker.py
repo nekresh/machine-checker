704f1cfc436a61521054362912e8d6a45e2ddfac
Guillaume Pouillet <nekresh@gmail.com>
2014-08-04 10:04:01 +0200

    Add mlx support and begin verbose output

--- checker.py
+++ checker.py
@@ -1,9 +1,10 @@
 #!env python3
 
 class logger:
-    def __init__(self):
+    def __init__(self, verbose=False):
         self._success = 0
         self._total = 0
+        self._verbose = verbose
 
     def success(self, job):
         self._success += 1
@@ -20,16 +21,26 @@ class logger:
         else:
             self.fail(job)
 
+    def verbose(self, cmd):
+        if self._verbose:
+            print(cmd)
+
     def __str__(self):
         return("%s success on %s tests (%.5s%%)" % (self._success, self._total, self._success / self._total * 100.0))
 
 class python_checker:
-    def check(self, logger, mode):
+    def __init__(self, logger):
+        self._logger = logger
+
+    def check(self, mode):
         import sys
-        logger.check(sys.version_info.major > 3 or (sys.version_info.major == 3 and sys.version_info.minor >= 3), "python>=3.3")
+        self._logger.check(sys.version_info.major > 3 or (sys.version_info.major == 3 and sys.version_info.minor >= 3), "python>=3.3")
 
 class program_checker:
-    def check(self, logger, mode):
+    def __init__(self, logger):
+        self._logger = logger
+
+    def check(self, mode):
         import shutil, os
         
         # use shutil.which when present
@@ -48,11 +59,11 @@ class program_checker:
                 for l in f:
                     binaries.append(l.strip())
         except:
-            logger.fail("mode %s" % mode)
+            self._logger.fail("mode %s" % mode)
             pass
 
         for p in binaries:
-            logger.check(w(p) is not None, p)
+            self._logger.check(w(p) is not None, p)
 
     def which(self, program):
         import os
@@ -63,7 +74,10 @@ class program_checker:
         return None
 
 class library_checker:
-    def check(self, logger, mode):
+    def __init__(self, logger):
+        self._logger = logger
+
+    def check(self, mode):
         import os, shlex
 
         libs = self.load(os.path.join("testlib", "libraries.json"))
@@ -82,13 +96,14 @@ class library_checker:
                                 args.append(item)
 
                     c = self.execute(args)
-                    logger.check(c, "[%s] check %s with %s" % (lang, file["name"], p))
+                    self._logger.check(c, "[%s] check %s with %s" % (lang, file["name"], p))
 
     def getAdditionalArgs(self, key, file, options):
+        import shlex
         entry = file[key]
         
         if type(entry) is dict and "$exec" in entry:
-            import shlex, subprocess
+            import subprocess
             args = shlex.split(entry["$exec"])
 
             try:
@@ -102,15 +117,20 @@ class library_checker:
             return shlex.split(output)
         elif type(entry) is list:
             return [ options[key] % item for item in entry ]
+        elif key in options:
+            return [ options[key] % file[key] ]
 
-        return [ options[key] % file[key] ]
+        return shlex.split(file[key])
 
     def execute(self, args):
         import subprocess
 
+        self._logger.verbose(' '.join(args))
         try:
             p = subprocess.Popen(args, stderr=subprocess.DEVNULL)
-            return p.wait() == 0
+            value = p.wait() == 0
+            self._logger.verbose("ret: %s" % value)
+            return value
         except OSError:
             pass
         return False
@@ -122,7 +142,10 @@ class library_checker:
             return json.load(f)
 
 class manpage_checker:
-    def check(self, logger, mode):
+    def __init__(self, logger):
+        self._logger = logger
+
+    def check(self, mode):
         import os
 
         manpages = self.load(os.path.join("mans", "manpages.json"))
@@ -130,11 +153,12 @@ class manpage_checker:
         for category, pages in manpages.items():
             for page in pages:
                 c = self.execute(category, page)
-                logger.check(c, "[man] %s %s" % (category, page))
+                self._logger.check(c, "[man] %s %s" % (category, page))
 
     def execute(self, category, page):
         import subprocess
 
+        self._logger.verbose("man %s %s" % (category, page))
         try:
             p = subprocess.Popen(["man", category, page], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             return p.wait() == 0
@@ -156,14 +180,14 @@ def main(args):
         mode = args[1]
 
     l = logger()
-    c = python_checker()
-    c.check(l, mode)
-    c = program_checker()
-    c.check(l, mode)
-    c = library_checker()
-    c.check(l, mode)
-    c = manpage_checker()
-    c.check(l, mode)
+    c = python_checker(l)
+    c.check(mode)
+    c = program_checker(l)
+    c.check(mode)
+    c = library_checker(l)
+    c.check(mode)
+    c = manpage_checker(l)
+    c.check(mode)
     
     print(l)
 
---
+++ testlib/C/mlx.c
@@ -0,0 +1,7 @@
+#include <mlx.h>
+
+int main(int ac, char** av)
+{
+	mlx_init();
+	return (0);
+}
